96a59b4dd9773a057975b2494f500b492c956374
fundamentos backend <fundamentos.backend.2023@outlook.com>
2025-07-12 22:43:03 -0600

    add: asynchronism code | read files | read from keyboard

--- callbacks.js
+++ callbacks.js
@@ -73,3 +73,70 @@ fetchBooksV2((booksv2) => {
 });
 
 
+//* Acciones anidadas - Callbacks anidados
+
+
+const booksv3 = {
+    book1 : {
+        title: 'El amor en los tiempos del cólera',
+        author: 'Gabriel García Márquez'
+    },
+    book2: {
+        title: 'Por quien doblan las campanas',
+        author: 'Ernest Hemingway'
+    },
+    book3: {
+        title: 'El extranjero',
+        author: 'Albert Camus'
+    }
+}
+
+const fetchBooksV3 = (actionToTake, secondActionToTake) => {
+    console.log('Statring books fetching');
+    setTimeout(() => {
+        myBooks = booksv3;
+        if(!myBooks){
+            console.log('something went wrong :(');
+        }
+        actionToTake(myBooks, secondActionToTake)
+    }, 2000);
+}
+
+fetchBooksV3((books, secondActionToTake) => {
+    console.log('Showing books collection');
+    console.log(books);
+    secondActionToTake()
+    },
+    function(){
+        setTimeout(() => {
+            console.log('We hope you like our books selection. Enjoy!');
+        }, 3000);
+});
+
+//* ¿Return asíncrono?
+
+const booksv4 = {
+    book1 : {
+        title: 'El amor en los tiempos del cólera',
+        author: 'Gabriel García Márquez'
+    },
+    book2: {
+        title: 'Por quien doblan las campanas',
+        author: 'Ernest Hemingway'
+    },
+    book3: {
+        title: 'El extranjero',
+        author: 'Albert Camus'
+    }
+}
+
+const retrieveBooks = () => {
+    setTimeout(() => {
+        const retrievedBooks = booksv4;
+        if(!retrieveBooks) console.log('Data not retrieve')
+        return retrievedBooks;
+    }, 1500);
+}
+
+const library = retrieveBooks();
+console.log(library);
